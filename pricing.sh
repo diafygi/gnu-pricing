90249299b03ee5f2021d5c9d8d0cab6a8a154475
Alex Burka <alex@alexburka.com>
2015-05-13 01:30:05 +0000

    remove $ARGS in favor of "$@"

--- pricing.sh
+++ pricing.sh
@@ -27,7 +27,6 @@ else
 fi
 
 #print pricing if requested
-ARGS="$@"
 for var in "$@"
 do
     if [ "$var" = "--pricing" ]
@@ -59,5 +58,5 @@ NEWCOUNT=$(( $USECOUNT + 1 ))
 echo "$NEWCOUNT" > "$HOME/.gnu-pricing/$THISCMD.usage"
 
 #run the command as normal
-$THISCMD $ARGS
+$THISCMD "$@"
 
